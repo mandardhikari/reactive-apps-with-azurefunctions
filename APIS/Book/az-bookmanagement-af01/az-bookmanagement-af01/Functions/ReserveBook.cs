eed362e434ec0b7734738360ed93912f525cf4ba
mandardhikari <mandardhikari@gmail.com>
2020-04-28 15:11:52 +0930

    Added Correlated logging to the Book Management service

--- APIS/Book/az-bookmanagement-af01/az-bookmanagement-af01/Functions/ReserveBook.cs
+++ APIS/Book/az-bookmanagement-af01/az-bookmanagement-af01/Functions/ReserveBook.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using az_bookmanagement_af01.Interfaces;
 using az_bookmanagement_af01.Models;
+using az_bookmanagement_af01.Constants;
 
 namespace az_bookreservation_af01.Functions
 {
@@ -27,21 +28,59 @@ namespace az_bookreservation_af01.Functions
             [HttpTrigger(AuthorizationLevel.Function, "Post")] HttpRequest httpRequest,
             ILogger logger)
         {
-            //Create a reservation request
+            // Read the body 
             try
             {
-                // Read the body 
                 string requestBody = await new StreamReader(httpRequest.Body).ReadToEndAsync();
                 EventSchema reservationEvent = JsonConvert.DeserializeObject<EventSchema>(requestBody);
 
-                await _sqlHelper.LockBook(reservationEvent).ConfigureAwait(false);
+                //Create a reservation request
+                try
+                {
+                    logger.LogInformation(new EventId(Convert.ToInt32(Logging.EventId.ReserveBook)),
+                      Logging.LoggingTemplate,
+                      reservationEvent.BookReservation.CorrelationID,
+                      nameof(ReserveBook),
+                      reservationEvent.EventType.ToString(),
+                      Logging.Status.Started.ToString(),
+                      "Updating Book status based on event"
+                      );
+
+                    await _sqlHelper.LockBook(reservationEvent).ConfigureAwait(false);
+
+                    logger.LogInformation(new EventId(Convert.ToInt32(Logging.EventId.ReserveBook)),
+                      Logging.LoggingTemplate,
+                      reservationEvent.BookReservation.CorrelationID,
+                      nameof(ReserveBook),
+                      reservationEvent.EventType.ToString(),
+                      Logging.Status.Succeeded.ToString(),
+                      "Updated Book status based on event"
+                      );
+
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(new EventId(Convert.ToInt32(Logging.EventId.ReserveBook)),
+                      Logging.LoggingTemplate,
+                      reservationEvent.BookReservation.CorrelationID,
+                      nameof(ReserveBook),
+                      reservationEvent.EventType.ToString(),
+                      Logging.Status.Failed.ToString(),
+                      string.Format("Failed updating Book status based on event.Exception {0}", ex.Message)
+                      );
+                    //Output Exception Event
+                }
 
             }
             catch (Exception ex)
             {
-                //Output Exception Event
+                logger.LogError(new EventId(Convert.ToInt32(Logging.EventId.ReserveBook)),
+                                        Logging.GenericExceptionLoggingTemplate,
+                                        nameof(ReserveBook),
+                                        Logging.Status.Failed.ToString(),
+                                        string.Format("Failed while parsing incoming event. Exception {0}", ex.Message)
+                                        );
             }
-
         }
     }
 }
